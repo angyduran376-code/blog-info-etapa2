44b237b50fec5de273e423bd1eb4301f8dc66bb6
unknown <angyduran376@gmail.com>
2025-12-12 22:26:08 -0300

    rama angy

--- blog/blog/configuraciones/prod.py
+++
@@ -1,6 +0,0 @@
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'g4informatorio@gmail.com'
-EMAIL_HOST_PASSWORD = 'Informatorio+5202'
